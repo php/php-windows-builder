c98918747fb27d673c83ff28da1282ecc81b2db8
Shivam Mathur <shivam_jpr@hotmail.com>
2024-04-04 14:01:17 +0530

    Add support for run tests after building the extension

--- extension/BuildPhpExtension/private/Invoke-Build.ps1
+++ extension/BuildPhpExtension/private/Invoke-Build.ps1
@@ -18,6 +18,9 @@ Function Invoke-Build {
         $bat_content += "call phpize 2>&1"
         $bat_content += "call configure --with-php-build=`"..\deps`" $($Config.options) --with-mp=`"disable`" --enable-debug-pack 2>&1"
         $bat_content += "nmake /nologo 2>&1"
+        if($env:RUN_TESTS -eq "true") {
+            $bat_content += "nmake /nologo test 2>&1"
+        }
         $bat_content += "exit %errorlevel%"
         Set-Content -Encoding "ASCII" -Path task.bat -Value $bat_content
 
