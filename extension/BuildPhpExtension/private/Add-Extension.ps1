e0a2a8b89c8c572586ddd5e8bfeefe3ec9e8a7d0
Shivam Mathur <shivam_jpr@hotmail.com>
2024-05-21 16:31:44 +0530

    Add logs for adding dependency extensions

--- extension/BuildPhpExtension/private/Add-Extension.ps1
+++ extension/BuildPhpExtension/private/Add-Extension.ps1
@@ -35,7 +35,15 @@ Function Add-Extension {
         Set-Content -Encoding "ASCII" -Path $Extension-task.bat -Value $bat_content
         $builder = "$currentDirectory\php-sdk\phpsdk-$($Config.vs_version)-$($Config.Arch).bat"
         $task = (Get-Item -Path "." -Verbose).FullName + "\$Extension-task.bat"
-        & $builder -t $task
+        $suffix = "php_" + (@(
+            $Config.name,
+            $Config.ref,
+            $Config.php_version,
+            $Config.ts,
+            $Config.vs_version,
+            $Config.arch
+        ) -join "-")
+        & $builder -t $task | Tee-Object -FilePath "build-$suffix.txt"
         $includePath = "$currentDirectory\php-dev\include"
         New-Item -Path $includePath\ext -Name $Extension -ItemType "directory" | Out-Null
         Get-ChildItem -Path (Get-Location).Path -Recurse -Include '*.h', '*.c' | Copy-Item -Destination "$includePath\ext\$Extension"
