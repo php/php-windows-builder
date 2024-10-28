60e6457413ef20f4c156f0ca80a3588979ecab0f
Shivam Mathur <shivam_jpr@hotmail.com>
2024-10-28 07:44:47 +0530

    Log temporary files cleanup

--- extension/BuildPhpExtension/private/Invoke-CleanupTempFIles.ps1
+++ extension/BuildPhpExtension/private/Invoke-CleanupTempFIles.ps1
@@ -4,7 +4,12 @@ Function Invoke-CleanupTempFiles {
 
     $newSystemFiles = Compare-Object -ReferenceObject $script:initialSystemState -DifferenceObject $currentSystemState -Property FullName | Where-Object {$_.SideIndicator -eq "=>"}
     $newUserFiles = Compare-Object -ReferenceObject $script:initialUserState -DifferenceObject $currentUserState -Property FullName | Where-Object {$_.SideIndicator -eq "=>"}
+    $tempFiles = $newSystemFiles + $newUserFiles
+    if($tempFiles.Count -gt 0) {
+        Write-Host "Cleaning up temporary files"
+    }
     $newSystemFiles + $newUserFiles | ForEach-Object {
+        Write-Host "Removing $($_.FullName)"
         Remove-Item -Path $_.FullName -Force
     }
 }
