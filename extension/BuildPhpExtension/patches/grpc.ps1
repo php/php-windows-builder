c80b87e82a64793755f4e4c36610698e6580f42f
Shivam Mathur <shivam_jpr@hotmail.com>
2025-02-03 10:44:58 +0530

    Update grpc patch to use /std:c++17

--- extension/BuildPhpExtension/patches/grpc.ps1
+++ extension/BuildPhpExtension/patches/grpc.ps1
@@ -10,5 +10,5 @@ function CreateFolderIfMissing(path) {
 "@
 (Get-Content config.w32) | ForEach-Object { $_.Replace('base_dir+"\\ext\\grpc', 'base_dir+"') } | Set-Content config.w32
 (Get-Content config.w32) | ForEach-Object { $_.Replace('FSO.CreateFolder', 'CreateFolderIfMissing') } | Set-Content config.w32
-(Get-Content config.w32) | ForEach-Object { $_ -replace '/D_WIN32_WINNT=0x600', '/D_WIN32_WINNT=0x600 /FS /std:c11' } | Set-Content config.w32
+(Get-Content config.w32) | ForEach-Object { $_ -replace '/D_WIN32_WINNT=0x600', '/D_WIN32_WINNT=0x600 /FS /std:c++17' } | Set-Content config.w32
 (Get-Content config.w32) | ForEach-Object { $_.Replace('ARG_WITH("grpc", "grpc support", "no");', $fn) } | Set-Content config.w32
