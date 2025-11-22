c270a6fb7f91d267210d8eaf5f4b13ef81209127
Shivam Mathur <shivam_jpr@hotmail.com>
2025-11-22 16:59:45 +0530

    Add patch for amqp for 8.5

---
+++ extension/BuildPhpExtension/patches/amqp.ps1
@@ -0,0 +1,1 @@
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/remicollet/php-amqp/977449987412a3d5c59a036dbab8b6d67764bb3e/amqp_connection_resource.c" -OutFile "amqp_connection_resource.c"
