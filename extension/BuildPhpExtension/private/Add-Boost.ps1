3ee8e65be03fc022cd91970f639e7e45f35ba864
Shivam Mathur <shivam_jpr@hotmail.com>
2026-01-02 14:05:56 +0530

    Improve adding boost library

--- extension/BuildPhpExtension/private/Add-Boost.ps1
+++ extension/BuildPhpExtension/private/Add-Boost.ps1
@@ -12,6 +12,7 @@ Function Add-Boost {
         $url = "https://archives.boost.io/release/1.72.0/source/boost_1_72_0.zip"
         Get-File -Url $url -OutFile "boost.zip"
         Expand-Archive -Path "boost.zip" -DestinationPath "../deps"
+        Rename-Item -Path "../deps/boost_1_72_0" -NewName "boost"
     }
     end {
     }
