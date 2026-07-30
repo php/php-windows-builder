df36fcec7ee586a5c80f8e3530711e18df6dbe7e
Shivam Mathur <shivam_jpr@hotmail.com>
2026-07-30 19:56:21 +0530

    Fix ddtrace libdatadog patch

--- extension/BuildPhpExtension/patches/ddtrace.ps1
+++ extension/BuildPhpExtension/patches/ddtrace.ps1
@@ -1,6 +1,2 @@
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/DataDog/dd-trace-php/master/datadog-windows.sym" -OutFile "datadog-windows.sym"
-Invoke-WebRequest -Uri "https://github.com/DataDog/libdatadog/archive/refs/heads/main.zip" -OutFile "$ENV:TEMP/libdatadog.zip"
-Remove-Item -Path "libdatadog" -Recurse -Force
-Expand-Archive -Path "$ENV:TEMP/libdatadog.zip" -DestinationPath "." -Force
-Rename-Item -Path "libdatadog-main" -NewName "libdatadog"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/DataDog/libdatadog/main/libdd-common/src/cc_utils.rs" -OutFile "libdatadog/tools/cc_utils/src/lib.rs"
+Remove-Item -Path "libdatadog/Cargo.toml" -Force
